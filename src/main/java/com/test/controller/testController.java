917b050199acf5b03e261a1b5610163835dbcae3
yechuyi <791098535@qq.com>
2020-07-16 16:06:34 +0800

    提交

---
+++ src/main/java/com/test/controller/testController.java
@@ -0,0 +1,11 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yechuyi
+ * 2020/7/16 15:25
+ * @version 1.0.0
+ **/
+public class testController {
+}
